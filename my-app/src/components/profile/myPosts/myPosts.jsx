1a04d8aa803f3732684212f953940709ccba506c
Alyona Mo~ <molenka13@gmail.com>
2023-01-04 14:57:54 +0200

    ref with button ADD POST

--- my-app/src/components/profile/myPosts/myPosts.jsx
+++ my-app/src/components/profile/myPosts/myPosts.jsx
@@ -5,15 +5,23 @@ import Post from './post/post';
 
 const MyPosts = (props) => {
 
+  let addPost = () => {
+    let text = newPostElement.current.value
+    alert(text)
+  }
+
+  let newPostElement = React.createRef()
+
   let postsElements = 
     props.posts.map(p => <Post className={classes.post} message={p.message} likes={p.likes}/>)
+
 return(
       <div className={classes.postsContent}>
         <div className={classes.postsBlock}>
         <div className={classes.myPosts}>My Posts</div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
         <div>
-        <button className={classes.addPost} onClick={alert('do u want to add post?')}>Add post</button>
+        <button className={classes.addPost} onClick={addPost}>Add post</button>
         </div>
         </div>
         <div className={classes.posts}>
